f5075abcb897546e552e8b794135be7dee61088f
Kim Thoenen <kim@smuzey.ch>
2013-08-06 17:40:00 +0200

    added upload destination

--- references/models.py
+++ references/models.py
@@ -1,8 +1,12 @@
 from django.db import models
+from time import time
+
+def give_file_name(instance, filename):
+    return '/'.join(['references', instance.user.username, time.time() + '_' + filename])
 
 class Reference(models.Model):
     donor_name = models.TextField()
-    donor_logo = models.ImageField()
-    donor_detail_img = models.ImageField()
+    donor_logo = models.ImageField(upload_to=give_file_name)
+    donor_detail_img = models.ImageField(upload_to=give_file_name)
     donor_title = models.TextField()
     reference_text = models.TextField()
