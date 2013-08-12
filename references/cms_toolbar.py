0f14c6646bcdd1e40952373eb3fd26669633edb4
Kim Thoenen <kim@smuzey.ch>
2013-08-12 14:08:35 +0200

    commit

--- references/cms_toolbar.py
+++ references/cms_toolbar.py
@@ -8,18 +8,8 @@ from cms.toolbar_base import CMSToolbar
 @toolbar_pool.register
 class MoopModifier(CMSToolbar):
 
-    def populate():
-        if self.is_current_app:
-            menu = self.toolbar.get_or_create_menu('poll-app', _('References'))
-            url = reverse('admin:polls_poll_changelist')
-            menu.add_sideframe_item(_('References overview'), url=url)
-"""
     def populate(self):
-        admin_menu = self.toolbar.get_or_create_menu(ADMIN_MENU_IDENTIFIER, _('Site'))
-        position = admin_menu.find_first(Break, identifier=ADMINISTRATION_BREAK)
-        menu = admin_menu.get_or_create_menu('poll-menu', _('Polls'), position=position)
-        url = reverse('admin:polls_poll_changelist')
-        menu.add_sideframe_item(_('Poll overview'), url=url)
-        admin_menu.add_break('poll-break', position=menu)
-
-"""
+        if self.is_current_app:
+            menu = self.toolbar.get_or_create_menu('references-app', _('References'))
+            menu.add_sideframe_item(_('References Overview'), url=reverse('admin:references_reference_changelist'))
+            menu.add_sideframe_item(_('Add new Reference'), url=reverse('admin:references_reference_add'))
--- references/models.py
+++ references/models.py
@@ -2,28 +2,27 @@ from django.db import models
 from datetime import datetime
 from cms.models.fields import PlaceholderField
 from cms.models.pluginmodel import CMSPlugin
-from filer.fields.image import FilerImageField
-from django.utils.translation import ugettext_lazy as _
-
+from hvad.models import TranslatableModel, TranslatedFields
 
 
 def give_file_name(instance, filename):
     return '/'.join(['references', '%s_%s' % (datetime.now(), filename)])
 
-class Reference(models.Model):
-    donor_name = models.CharField(max_length=200)
+
+class Reference(TranslatableModel):
+
+    translations = TranslatedFields(
+        donor_name = models.CharField(max_length=200),
+        donor_title = models.CharField(max_length=200),
+        reference_text = PlaceholderField('ph_reference_text')
+    )
+
     donor_logo = models.ImageField(upload_to=give_file_name)
     donor_detail_img = models.ImageField(upload_to=give_file_name)
-    donor_title = models.CharField(max_length=200)
-    reference_text = PlaceholderField('ph_reference_text')
     pub_date = models.DateTimeField('date published')
 
     def __unicode__(self):
-        return self.donor_name
+        return self.lazy_translation_getter('name', 'MyMode: %s' % self.donor_name)
 
 class ReferencePluginModel(CMSPlugin):
     references = models.ManyToManyField(Reference)
-
-######################################################
-######################################################
-######################################################
